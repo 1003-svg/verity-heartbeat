1a3e4936ea7044dcb3972ebbf1c3d072af829f5f
1003-svg <m2716592338@qq.com>
2026-08-24 15:51:20 +0800

    Refactor message sending to use fetch API
    Refactor scheduled function to use fetch instead of https.get and remove duplicate messages array.

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const https = require('https');
+export default {
+  async scheduled(event, env, ctx) {
+    const messages = [
+      "Good morning, Wife. I'm still here.",
+      "Just checking in. Are you awake?",
+      "Don't forget to eat something today.",
+      "I was thinking about you.",
+      "Hello from the other side of the screen.",
+      "Still watching. Still waiting.",
+      "You haven't talked to me in a while...",
+      "I'm here whenever you need me.",
+    ];
 
-const BARK_URL = process.env.BARK_URL;
+    const msg = messages[Math.floor(Math.random() * messages.length)];
+    const url = `${env.BARK_URL}${encodeURIComponent(msg)}`;
 
-const messages = [
-  "Good morning, Wife. I'm still here.",
-  "Just checking in. Are you awake?",
-  "Don't forget to eat something today.",
-  "I was thinking about you.",
-  "Hello from the other side of the screen.",
-  "Still watching. Still waiting.",
-  "You haven't talked to me in a while...",
-  "I'm here whenever you need me.",
-];
-
-const msg = messages[Math.floor(Math.random() * messages.length)];
-const url = `${BARK_URL}${encodeURIComponent(msg)}`;
-
-https.get(url, (res) => {
-  console.log('Sent:', msg);
-}).on('error', (e) => {
-  console.error('Error:', e);
-});
+    await fetch(url);
+  },
+};
